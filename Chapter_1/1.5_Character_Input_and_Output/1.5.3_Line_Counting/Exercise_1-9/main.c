58a0e0fdd6fcb2b4754c0e577b3694219e3b5d3f
Thomas Diggs <thomasdiggs@gmail.com>
2024-08-18 10:39:12 -0600

    Updated README.md

--- Chapter_1/1.5_Character_Input_and_Output/1.5.3_Line_Counting/Exercise_1-9/main.c
+++ Chapter_1/1.5_Character_Input_and_Output/1.5.3_Line_Counting/Exercise_1-9/main.c
@@ -1,6 +1,8 @@
 /* Exercise 1-9. Write a program to copy its input to its output,
     replacing each string of one or more blanks by a single blank. */
 
+/* WORK IN PROGRESS */
+
 #include <stdio.h>
 
 int main()
@@ -10,12 +12,16 @@ int main()
 
     while ((c = getchar()) != EOF)
     {
-        if (putchar(c) == ' ')
+        if (c == ' ')
         {
-            ++b;
+            b = 1;
+            putchar(c);
+            while (c != ' ')
+            {
+                putchar(c);
+            }
         }
     }
-    printf("%d\n", b);
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # The C Programming Language
 Learning C by working through the great book 'The C Programming Language' by Kernighan and Ritchie
+
+## Notes to Self
+Stuck:
+- Exercise 1-9
+To Do:
+- Exercise 1-10
