8aa94c89a5f5ff555fa803f10ce288609aa2aab3
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-19 09:43:37 -0600

    Chapter_1/1.5_Character_Input_and_Output/1.5.3_Line_Counting/Exercise_1-10 complete

---
+++ Chapter_1/1.5_Character_Input_and_Output/1.5.3_Line_Counting/Exercise_1-10/main.c
@@ -0,0 +1,36 @@
+/* Write a program to copy its input to its output, replacing each tab by \t, each
+ * backspace by \b, and each backslash by \\. This makes tabs and backspaces visible in an
+ * unambiguous way.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+        {
+            putchar('\\');
+            putchar('t');
+        }
+        else if (c == '\b')
+        {
+            putchar('\\');
+            putchar('b');
+        }
+        else if (c == '\\')
+        {
+            putchar('\\');
+            putchar('\\');
+        }
+        else
+        {
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
