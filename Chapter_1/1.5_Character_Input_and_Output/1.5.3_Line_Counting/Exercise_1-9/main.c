6a5edb46fab1e472fa95e49f0bf5fb49abd3678c
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-19 09:34:09 -0600

    Chapter_1/1.5_Character_Input_and_Output/1.5.3_Line_Counting/Exercise_1-9 complete

--- Chapter_1/1.5_Character_Input_and_Output/1.5.3_Line_Counting/Exercise_1-9/main.c
+++ Chapter_1/1.5_Character_Input_and_Output/1.5.3_Line_Counting/Exercise_1-9/main.c
@@ -1,8 +1,6 @@
 /* Exercise 1-9. Write a program to copy its input to its output,
     replacing each string of one or more blanks by a single blank. */
 
-/* WORK IN PROGRESS */
-
 #include <stdio.h>
 
 int main()
@@ -14,12 +12,16 @@ int main()
     {
         if (c == ' ')
         {
-            b = 1;
-            putchar(c);
-            while (c != ' ')
+            if (b == 0)
             {
                 putchar(c);
+                b = 1;
             }
+        }
+        else
+        {
+            putchar(c);
+            b = 0;
         }
     }
 
