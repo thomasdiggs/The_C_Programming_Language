5955d2c1daed397126f069474418b80a65235ea0
Thomas Diggs <thomasdiggs@gmail.com>
2024-10-31 20:37:43 -0600

    Pushing what's there

---
+++ Chapter_1/1.6_Arrays/Exercise_1-13/main.c
@@ -0,0 +1,11 @@
+/* Exercise 1-13. Write a program to print a histogram of the lengths of words in its input. It is
+easy to draw the histogram with the bars horizontal; a vertical orientation is more challenging. */
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+
+    return 0;
+}
---
+++ Chapter_1/1.6_Arrays/Exercise_1-14/main.c
@@ -0,0 +1,11 @@
+/* Exercise 1-14. Write a program to print a histogram of the frequencies of different characters
+in its input. */
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+
+    return 0;
+}
